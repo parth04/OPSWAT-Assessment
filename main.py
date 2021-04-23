2efa8ba0ce6eba4111624821707737f45d2e7313
parth04 <parth.gun@gmail.com>
2021-04-22 22:44:39 -0400

    Updated the code

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import  properties
 import json
-
+import time
 import requests
 import hashlib
 import sys
 
 
-def printResult(file, response):
+# encoded = None
+# def createHash(file):
+#     global encoded
+#     BLOCKSIZE = 65536
+#     MD5 = hashlib.md5()
+#     with open(file, 'rb') as afile:
+#         buf = afile.read(BLOCKSIZE)
+#         while len(buf) > 0:
+#             MD5.update(buf)
+#             buf = afile.read(BLOCKSIZE)
+#     encoded = MD5.hexdigest()
+def printResult(file, response): #This is a printing function to display the scan result in desired format
     print("------------------Result------------------")
     print("filename: ", file)
     print("overall_status: ", response["scan_results"]["scan_all_result_a"])
@@ -24,8 +32,8 @@ def printResult(file, response):
         print("scan_result: ", scans[i]["scan_result_i"])
         print("def_time: ", scans[i]["def_time"])
 
-sha256 = hashlib.sha256()
-api_key = "<Enter your own API key>"
+# sha256 = hashlib.sha256() #This is a constructor method in hashlib package which will used to create SHA256 hash object
+
 if len (sys.argv) < 2:
     print("ERROR! Please specify the target file.")
     print("For example: python3 main.py <FILE_NAME> \n")
@@ -33,35 +41,48 @@ elif len(sys.argv) > 2:
     print("ERROR! Too many files provided. Please submit only single file.")
     print("For example: python3 main.py <FILE_NAME> \n")
 else:
-    with open(sys.argv[1], "rb") as data:
-        while True:
-            line = data.read()
-            if not line:
-                break
-            sha256.update(line)
-    encoded = sha256.hexdigest()
-    # print("encoded = ",encoded)
+    # createHash(sys.argv[1])
+    with open(sys.argv[1], "rb") as data: #We read the file in "rb" mode because update function below take only bytes-like object
+        # while True:
+        line = data.read()
+            # if not line:
+            #     break
+            # sha256.update(line) #It will update the hash as every line reads
+    encoded = hashlib.sha256(line).hexdigest() #This will give string conatining only hexadecimal digits
+    print("MD5 = ", encoded)
 
 
-    response = requests.get("https://api.metadefender.com/v4/hash/" + encoded, headers = {'apikey':api_key}).json()
-    # print("response = ", response["appinfo"])
-
-    if len(response) < 2:
+    response = requests.get(url=properties.getByHash + encoded, headers = {'apikey':properties.api_key}) #Checking if SHA256 hash is present in OPSWAT database
+    if response.status_code != 200:
+        response = response.json()
         print("Hash not found!")
         print("Uploading "+sys.argv[1]+" hash to OPSWAT database....")
-        with open(sys.argv[1],"rb") as data:
-            response = requests.post(url='https://api.metadefender.com/v4/file', headers = {'apikey':api_key}, files = {'file':data}).json()
-            data_id = response['data_id']
-            print("Scanning....")
-            while True:
-                response = requests.get(url='https://api.metadefender.com/v4/file/' + data_id, headers = {'apikey':api_key}).json()
-                if "scan_results" not in response or response["scan_results"]["progress_percentage"] == 100:
-                    break
 
-                print("Done!")
-            printResult(sys.argv[1], response)
+        with open(sys.argv[1],"rb") as data:
+            line = data.read()
+            response = requests.post(url=properties.fileUpload, headers = {'apikey':properties.api_key, 'content-type':'application/octet-stream'}, data = line) #Uploading the file to OPSWAT
+            print("Uploaded file status code", response.status_code)
+            response.raise_for_status()
+            responseJson = response.json()
+
+            # print("Uploaded file response= ", responseJson)
+
+            data_id = responseJson['data_id']
+            print("dataId = ", data_id)
+            while True:
+                response = requests.get(url=properties.getByID + data_id, headers = {'apikey':properties.api_key})
+                response.raise_for_status()
+                responseJson = response.json()
+                # print("Response of getByID = ", responseJson)
+                if responseJson["scan_results"]["progress_percentage"] == 100: #Checking the condition if scanning completed to 100%
+                    break
+                print("Scanning... ", responseJson["scan_results"]["progress_percentage"])
+                time.sleep(5) #To avoid excessive API calls
+            print("Done!")
+            printResult(sys.argv[1], responseJson)
     else:
         print("Hash Found!")
+        response = response.json()
         printResult(sys.argv[1], response)
 
 
